295da2fc83ddee2f7af7cbd5046b6158b144144e
Brendonexus <Brendonexus@gmail.com>
2017-10-26 19:57:24 +0100

    Added comments

--- core.py
+++ core.py
@@ -17,3 +17,18 @@ if __name__ == "__main__":
             mainfunction(source)
             if mainfunction == True:
                 print ("This is true")
+
+
+"""
+
+This script is meant to consistently listen for a trigger word. In this case 'hello'.
+
+Once the trigger word has been processed the script would in some way, authenticate and then 
+pass to another function where more commands could be passed. If the trigger word is not
+authenticated then the system should keep listening and do nothing other then print to the 
+console. 
+
+Another issue I am facing is that Google Speech throws an error when it picks up voice 
+that it does not recognise. I need to find a way to catch this and restart the function.
+ 
+"""
